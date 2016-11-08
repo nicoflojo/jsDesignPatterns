9b8c2b9bd9c3d0fad4db07b96506334c520c4ce2
nico@yoursole.com <nicoflojo.developer@gmail.com>
2016-11-08 12:20:02 -0800

    finishes contructor adds module

--- contructor-pattern.js
+++ contructor-pattern.js
@@ -58,3 +58,21 @@ Object.defineProperties(newObject, {
     writable: false
   }
 });
+
+// Constructors with prototype
+function Car(model, year, miles) {
+  this.model = model;
+  this.year = year;
+  this.miles = miles;
+};
+
+// Note: uses Object.prototype.newMethod rather than Object.prototype
+Car.prototype.toString = function() {
+  return this.model + " has done " + this.miles + " miles";
+};
+
+var civic = new Car("Honda Civic", 2009, 20000);
+var mondeo = new Car("Ford Mondeo", 2010, 5000);
+
+console.log(civic.toString());
+console.log(mondeo.toString());
