617e63e418c6a967d5f7d2b21ca35b84cf7c9fc9
nico@yoursole.com <nicoflojo.developer@gmail.com>
2016-11-21 11:06:03 -0800

    finishes mediator pattern

--- mediator-pattern.js
+++ mediator-pattern.js
@@ -1,5 +1,4 @@
 // Mediator Pattern
-
 var orgChart = {
   addNewEmployee: function() {
     var employeeDetail = this.getEmployeeDetail();
@@ -12,3 +11,27 @@ var orgChart = {
   },
   // ...
 }
+
+// Mediator set up as event aggregato
+
+var MenuItem = MyFrameworkView.extend({
+  events: {
+    "click .thatThing": "clickedIt"
+  },
+  clickedIt: function(e) {
+    e.preventDefault();
+    MyFramework.trigger("menu:click:" + this.model.get("name"));
+  }
+});
+
+// Somewhere else
+
+var MyWorkflow = fucntion() {
+  myFramework.on("menu:click:foo", this.doStuff, this);
+};
+
+MyWorkflow.prototype.doStuff = function() {
+  // instantiate multiple objects.
+  // set up event handlers for those objects.
+  // coordinate all of the objects into meaningful workflow.
+};
