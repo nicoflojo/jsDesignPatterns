bf6d8f65ec10949f01b01ea6053463cc3311f024
nico@yoursole.com <nicoflojo.developer@gmail.com>
2016-11-21 10:47:50 -0800

    first commit monday head up buddy

---
+++ mediator-pattern.js
@@ -0,0 +1,14 @@
+// Mediator Pattern
+
+var orgChart = {
+  addNewEmployee: function() {
+    var employeeDetail = this.getEmployeeDetail();
+    employeeDetail.on("complete", function(employee) {
+      var managerSelector = this.selectManager(employee);
+      managerSelector.on("save", function(employee) {
+        employee.save();
+      });
+    });
+  },
+  // ...
+}
